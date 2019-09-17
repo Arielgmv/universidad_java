6864885d810e5386180be8c3546e70c943072d4c
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-09-17 12:48:23 -0400

    Avances_(17-9-19)

---
+++ PalabraThis/src/PalabraThis.java
@@ -0,0 +1,22 @@
+public class PalabraThis {
+    public static void main(String[] args) {
+        Persona persona = new Persona("Juan");
+    }
+}
+class Persona{
+    String nombre;
+    Persona(String nombre){
+        this.nombre = nombre;//this apunta a un objeto de tipo persona
+
+        //imprimir el objeto persona
+        Imprimir imprimir = new Imprimir();
+        imprimir.imprimir(this);//this contiene una referencia al objeto Persona
+    }
+}
+
+class Imprimir{
+    public void imprimir(Persona p){
+        System.out.println("impresion argumento: " + p);//valor del objeto persona
+        System.out.println("impresion objeto actual (this)" + this);//this apunta a un objeto de tipo imprimir
+    }
+}
