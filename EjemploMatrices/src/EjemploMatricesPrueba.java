4185a8a5dfd1ab27844fa50048f16419006f43a7
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-10-07 17:24:47 -0400

    Avances_(7-10-19)_1

--- EjemploMatrices/src/EjemploMatricesPrueba.java
+++ EjemploMatrices/src/EjemploMatricesPrueba.java
@@ -1,9 +1,36 @@
 public class EjemploMatricesPrueba {
     public static void main(String[] args) {
-        //2. Declaramos una matriz de int
+        //1. Declaramos una matriz de int
         int edades [][];
         //2. Instaciar una matriz
         edades = new int[3][2];
-        //3. 
+        //3. Instanciar los valores de la matriz
+        edades[0][0] = 30;
+        edades[0][1] = 15;
+        edades[1][0] = 20;
+        edades[1][1] = 45;
+        edades[2][0] = 5;
+        edades[2][1] = 38;
+
+        //4. Imprimir
+        System.out.println("Matriz de enteros indice [0] [0]: " + edades[0][0]);
+        System.out.println("Matriz de enteros indice [2] [0]: " + edades[2][0]);
+
+        //1. Declarar e instanciar una matriz de objetos de tipo persona
+        Persona personas[][] = new Persona[1][2];
+
+        //2. Inicializamos valores
+        personas[0][0] = new Persona("Juan");
+        personas[0][1] = new Persona("Carla");
+
+        //3. imprimir valores
+        System.out.println("Matriz persona indice [0] [0]: " + personas[0][0]);
+        System.out.println("Matriz persona indice [0] [0]: " + personas[0][1]);
+
+        for(int renglon = 0; renglon < personas.length; renglon++){
+            for(int columna = 0; columna < personas[renglon].length; columna++){
+                System.out.println("Matriz personas indice: " + renglon + " - " + columna + ": " + personas[renglon][columna]);
+            }
+        }
     }
 }
--- EjemploMatrices/src/Persona.java
+++ EjemploMatrices/src/Persona.java
@@ -1,21 +1,21 @@
 public class Persona {
     private String nombre;
 
-    public Persona(String nombre) {
+    public Persona(String nombre){
         this.nombre = nombre;
     }
 
-    public String getNombre() {
+    public String getNombre(){
         return nombre;
     }
 
-    public void setNombre(String nombre) {
+    public void setNombre(String nombre){
         this.nombre = nombre;
     }
 
     @Override
-    public String toString() {
-        return "Persona{" +
+    public String toString(){
+        return "Persona {" +
                 "nombre=" + nombre +
                 '}';
     }
