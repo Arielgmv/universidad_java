d0fb5d53e7cf2db93ec98dd11ac513768b98afd2
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-10-12 07:34:42 -0400

    Avances_(12-10-19)_1

---
+++ MundoProyecto/src/com/gm/mundopc/Computadora.java
@@ -0,0 +1,9 @@
+package com.gm.mundopc;
+
+public class Computadora {
+    private int idComputadora;
+    private String nombre;
+    private String monitor;
+    private String teclado;
+    private String raton;
+}
