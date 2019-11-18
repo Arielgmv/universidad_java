9790d0e19d910d3aeff365c541aa2a37f691cbdb
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-11-18 18:10:46 -0400

    Avances_(18-11-19)

--- EjercicioClaseObject/src/main/java/test/ClaseObject.java
+++ EjercicioClaseObject/src/main/java/test/ClaseObject.java
@@ -5,11 +5,38 @@ import dominio.Empleado;
 public class ClaseObject {
     public static void main(String[] args) {
         Empleado emp1 = new Empleado("Juan", 10000);
+        //System.out.println("emp1 = " + emp1);
+        Empleado emp2 = new Empleado("juan", 10000);
+        //System.out.println("emp2 = " + emp2);
+        
+        compararObjetos(emp1, emp2);        
+    }
+    
+    private static void compararObjetos(Empleado emp1, Empleado emp2){
         System.out.println("emp1 = " + emp1);
-        Empleado emp2 = new Empleado("Juan", 10000);
         System.out.println("emp2 = " + emp2);
         
-        System.out.println("objetos iguales: " + (emp1 == emp2));        
+        //Revisión por referencia
+        if(emp1 == emp2){
+            System.out.println("Los objetos tienen la misma dirección en memoria");
+        }else{
+            System.out.println("Los objetos tienen distinta dirección en memoria");
+        }
         
+        //Revisión del método equals
+        if(emp1.equals(emp2)){
+            System.out.println("Los objetos tienen el mismo contenido");
+        }else{
+            System.out.println("Los objetos NO tienen el mismo contenido");
+        }
+        
+        //Revisamos el método hashCode
+        if(emp1.hashCode() == emp2.hashCode()){
+            System.out.println("Los objetos tienen el mismo codigo hash");
+            System.out.println("emp1 = " + emp1.hashCode());
+            System.out.println("emp2 = " + emp2.hashCode());
+        }else{
+            System.out.println("Los objetos NO tienen el mismo codigo hash");
+        }
     }
 }
