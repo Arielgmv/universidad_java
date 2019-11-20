1e67c51ef1ee5f82f7230fa7dd0598fa77ee6096
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-11-20 18:37:26 -0400

    Avances_(20-11-19)

---
+++ ManejoInterfaces/src/main/java/manejointerfaces/ManejoInterfaces.java
@@ -0,0 +1,16 @@
+package manejointerfaces;
+
+import datos.*;
+
+public class ManejoInterfaces {
+    public static void main(String[] args) {
+        AccesoDatos datos = new ImplementacionOracle();
+        datos.insertar();
+        
+        datos = new ImplementacionMySql();
+        datos.insertar();
+        datos.listar();
+        
+        System.out.println(AccesoDatos.MAX_REGISTROS);        
+    }
+}
