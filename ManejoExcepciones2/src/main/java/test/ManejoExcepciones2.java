cd19c6f23d67dac3cce5b60121fc9cb4959e5ba9
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-11-28 17:57:06 -0400

    Avances_(28-11-19)

---
+++ ManejoExcepciones2/src/main/java/test/ManejoExcepciones2.java
@@ -0,0 +1,48 @@
+package test;
+
+import datos.*;
+import excepciones.*;
+
+public class ManejoExcepciones2 {
+    public static void main(String[] args) {
+        AccesoDatos datos = new ImplementacionMySql();
+        datos.simularError(false);
+        ejecutar(datos, "listar");
+        
+        AccesoDatos datos2 = new ImplementacionOracle();
+        datos.simularError(false);
+        ejecutar(datos2, "insertar");
+    }
+    
+    private static void ejecutar(AccesoDatos datos, String accion){
+        if("listar".equals(accion)){
+            try {
+                datos.listar();
+            } catch (LecturaDatosEx ex) {
+                System.out.println("Error1 lectura");
+                ex.printStackTrace(System.out);
+            } catch (AccesoDatosEx ex) {
+                System.out.println("Error acceso a datos");
+                ex.printStackTrace(System.out);
+            }catch(Exception ex){
+                System.out.println("Error general");
+                ex.printStackTrace(System.out);
+            }
+            finally{
+                System.out.println("Procesar finally es opcional, pero siempre se va a ejecutar");
+            }
+        }else if("insertar".equals(accion)){
+            try {
+                datos.insertar();
+            } catch (AccesoDatosEx ex) {
+                System.out.println("Excepción de acceso a datos");
+                ex.printStackTrace(System.out);
+            }
+            finally{
+                System.out.println("Procesar finally es opcional");
+            }
+        }else{
+            System.out.println("No se proporciono ninguna acción conocida");
+        }
+    }
+}
