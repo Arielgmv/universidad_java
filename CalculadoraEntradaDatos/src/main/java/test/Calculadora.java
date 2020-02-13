2612e933d185dc486389da1d86224d179ebc3da3
Arielgmv <ariel.munoz.villegas@gmail.com>
2020-02-12 20:41:19 -0400

    Avances_(12-2-20)

--- CalculadoraEntradaDatos/src/main/java/test/Calculadora.java
+++ CalculadoraEntradaDatos/src/main/java/test/Calculadora.java
@@ -1,7 +1,7 @@
 package test;
 
 import java.util.Scanner;
-import static calculadora.Operaciones.sumar;
+import static calculadora.Operaciones.*;
 
 public class Calculadora {
     public static void main(String[] args) {
@@ -10,7 +10,13 @@ public class Calculadora {
         int a = scanner.nextInt();
         System.out.println("Proporciona el segundo valor");
         int b = scanner.nextInt();
-        int resultado = sumar(a, b);
-        System.out.println("El resultado de la suma es: " + resultado);
+        int suma = sumar(a, b);
+        int resta = restar(a, b);
+        int multiplicacion = multiplicar(a, b);
+        int division = dividir(a, b);
+        System.out.println("El resultado de la suma es: " + suma);
+        System.out.println("El resultado de la resta es: " + resta);
+        System.out.println("El resultado de la multiplicación es: " + multiplicacion);
+        System.out.println("El resultado de la división es: " + division);
     }
 }
